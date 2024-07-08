7176195ea634c739b3a9580811d39c9f558ffc4b
TerryChengPobl <terry.cheng@poblgroup.co.uk>
2024-07-08 09:59:59 +0100

    chore: Update package.json version to 1.0.1 and add "up" script for building and publishing

--- src/classes/ErrorCap.ts
+++ src/classes/ErrorCap.ts
@@ -1,6 +1,7 @@
 export class ErrorCap {
   private static instance: ErrorCap;
   private connectionString: string;
+  private projectName: string;
 
   constructor() {}
 
@@ -11,13 +12,22 @@ export class ErrorCap {
     return ErrorCap.instance;
   }
 
-  public async init(connectUrl: string) {
+  public async init(
+    projectName: string,
+    connectUrl: string = "http://localhost:3000"
+  ): Promise<boolean> {
+    if (projectName === "" || connectUrl === "")
+      throw new Error("Invalid project name or connection string");
+    this.projectName = projectName;
     this.connectionString = connectUrl;
     console.log("ErrorCap Initialised");
+    return true;
   }
 
   public async sendError(err: Error, code: number = 500) {
-    console.log(`Error will be sent to ${this.connectionString}`);
+    console.log(
+      `Error will be sent to ${this.connectionString} with project name ${this.projectName}`
+    );
     console.log("Error sent with message: ", err.message, " and code: ", code);
   }
 }
